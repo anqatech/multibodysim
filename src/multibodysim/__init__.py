0b25b4706e9d671cfff28be6526d3e8f68e0a67d
Jalal El Hazzat <jalal@anqatech.com>
2026-04-28 14:43:56 +0100

    Remove stale flexible exports from package init

--- src/multibodysim/__init__.py
+++ src/multibodysim/__init__.py
@@ -4,8 +4,6 @@ from .rigid.rigid_symbolic_7part import Rigid7PartSymbolicDynamics
 from .rigid.rigid_simulator_7parts import Rigid7PartSimulator
 from .beam.cantilever_beam import CantileverBeam
 from .beam.clamped_clamped_beam import ClampedClampedBeam
-from .flexible.flexible_symbolic_model import FlexibleSymbolicDynamics
-from .flexible.flexible_simulator import FlexibleSimulator
 from .flexible.flexible_ns_dynamics import FlexibleNonSymmetricDynamics
 from .flexible.flexible_ns_simulator import FlexibleNonSymmetricSimulator
 from .controllers.pd_attitude import PlanarAttitudeController
